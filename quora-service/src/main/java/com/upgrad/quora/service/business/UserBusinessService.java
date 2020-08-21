7aec39a899d64397abfbe24634393063a705ead1
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-08-21 16:03:06 +0530

    introducing new Signup feature

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -0,0 +1,35 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.User;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public User signup(User user) throws SignUpRestrictedException {
+
+        String password = user.getPassword();
+        String[] encryptedText = cryptographyProvider.encrypt(user.getPassword());
+        user.setSalt(encryptedText[0]);
+        user.setPassword(encryptedText[1]);
+        if (userDao.getUserByUserName(user.getUserName()) != null) {
+            throw new SignUpRestrictedException("SGR-001", "Try any other Username, this Username has already been taken");
+        }
+        if (userDao.getUserByEmail(user.getEmail()) != null) {
+            throw new SignUpRestrictedException("SGR-002", "This user has already been registered, try with any other emailId");
+        }
+        return userDao.createUser(user);
+    }
+}
