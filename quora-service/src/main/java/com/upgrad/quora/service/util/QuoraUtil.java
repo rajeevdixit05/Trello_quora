ca7f871486f83f0e90a96f904b26db296569b122
Rajeev Dixit <rajeev.dixit@myntra.com>
2020-08-22 20:34:03 +0530

    add missing util package for QuoraUtil

---
+++ quora-service/src/main/java/com/upgrad/quora/service/util/QuoraUtil.java
@@ -0,0 +1,7 @@
+package com.upgrad.quora.service.util;
+
+public interface QuoraUtil {
+
+    public static final Long EIGHT_HOURS_IN_MILLIS = 8 * 60 * 60 * 1000L;
+    public static final String BEARER_TOKEN = "Bearer ";
+}
