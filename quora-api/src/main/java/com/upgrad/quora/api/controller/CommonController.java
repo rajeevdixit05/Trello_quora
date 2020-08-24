2a8f4eca1ddff3c6cbff1f0ebf197a7d4b4f7442
rajeevdixit05 <dixit.rajeev05@gmail.com>
2020-08-24 11:49:59 +0530

    adding Controller controller documentation

--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -1,4 +1,10 @@
 package com.upgrad.quora.api.controller;
 
+/**
+ * This endpoint is used to get the details of any user in the Quora Application.
+ * Also this endpoint can be accessed by any user in the application.
+ *
+ * @return all the details of the user from the database in the JSON response with the corresponding HTTP status.
+ */
 public class CommonController {
 }
